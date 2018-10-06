273ef528505e38a18b7c7fa63c3426d5b8299f60
waiyanphyohein <waiyanphyohein@gmail.com>
2018-10-05 22:30:10 -0500

    Implemented Login Page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import NavBar from './navigation.js';
+import Loginscreen from './Loginscreen'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      uploadScreen:[]
+    }
+  }
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<Loginscreen parentContext={this}/>);
+    this.setState({
+                  loginPage:loginPage
+                    })
+  }
   render() {
     return (
       <div className="App">
-      < NavBar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a           
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.loginPage}
+        {this.state.uploadScreen}
       </div>
     );
   }
 }
-
+const style = {
+  margin: 15,
+};
 export default App;
