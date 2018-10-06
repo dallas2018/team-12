90371ca33c993d54c0629ab23a3dc43cc72e517e
Tanzim Ali <tanzim_rayhan@yahoo.com>
2018-10-06 06:06:17 -0400

    We live V2

--- src/MenuPage.js
+++ src/MenuPage.js
@@ -9,22 +9,22 @@ class Menu extends Component {
         <div className="Menu">
         <Carousel>
   <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src ="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png" />
-    <Carousel.Caption>
+    <img width={600} height={500} alt="900x500" src ="https://slack-imgs.com/?c=1&url=https%3A%2F%2Fcdn-image.realsimple.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fportrait_435x518%2Fpublic%2F1527198293%2Fbest%2520beauty%2520products%2520and%2520must%2520haves%2520of%2520all%2520time.jpg%3Fitok%3Dmp4Ks9KD" />
+    <Carousel.Caption className="Caption">
       <h3>First slide label</h3>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png" />
-    <Carousel.Caption>
+    <img width={600} height={500} alt="900x500" src="https://slack-imgs.com/?c=1&url=https%3A%2F%2Fcdn2.stylecraze.com%2Fwp-content%2Fuploads%2F2014%2F02%2FBest-Popular-Hair-Care-Products-Available-In-India.jpg" />
+    <Carousel.Caption className="Caption">
       <h3>Second slide label</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png" />
-    <Carousel.Caption>
+    <img width={900} height={500} alt="900x500" src="https://i5.walmartimages.com/asr/407a9e0e-0d71-4676-a2aa-0894bcaee5a1_1.43e2cf366f595a6414b72dac06558ba3.jpeg" />
+    <Carousel.Caption className="Caption">
       <h3>Third slide label</h3>
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
@@ -42,7 +42,7 @@ class Menu extends Component {
           <Grid>
   <Row>
     <Col xs={6} md={4}>
-      <Thumbnail src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png" alt="242x200">
+      <Thumbnail src="https://i5.walmartimages.com/asr/407a9e0e-0d71-4676-a2aa-0894bcaee5a1_1.43e2cf366f595a6414b72dac06558ba3.jpeg" alt="242x200">
         <h3>Thumbnail label</h3>
         <p>Description</p>
         <p>
--- src/navigation.js
+++ src/navigation.js
@@ -25,13 +25,9 @@ class Navigation extends Component{
     <NavItem eventKey={4} href="/Login/">
       Sign Up/Log In
     </NavItem>
-    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-      <MenuItem eventKey={3.1}>Action</MenuItem>
-      <MenuItem eventKey={3.2}>Another action</MenuItem>
-      <MenuItem eventKey={3.3}>Something else here</MenuItem>
-      <MenuItem divider />
-      <MenuItem eventKey={3.4}>Separated link</MenuItem>
-    </NavDropdown>
+    <NavItem eventKey={5} href="/Donate/">
+    Donate
+    </NavItem>
   </Nav>
         </Navbar>);
     }
