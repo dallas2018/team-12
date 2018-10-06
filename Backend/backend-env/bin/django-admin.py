b96f9e882df60802a0b8073162c672b794ecf18e
Bryan Lliguicota <bryanlliguicota@Bryans-MacBook-Pro-2.local>
2018-10-05 20:06:23 -0500

    first commit

---
+++ Backend/backend-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bryanlliguicota/Desktop/CodeForGood2018/Backend/team-12/Backend/backend-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
