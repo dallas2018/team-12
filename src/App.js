377e27778bf26d863ad4e2fa75cc427416e39c36
Tanzim Ali <tanzim_rayhan@yahoo.com>
2018-10-05 21:49:34 -0400

    Fixed Nav version-1

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      < NavBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a>
-           < NavBar />
+            
+           
+           
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
