72acd5ac06edbe1d9e25fd32bc514482efbd1d9a
Tanzim Ali <tanzim_rayhan@yahoo.com>
2018-10-06 02:39:23 -0400

    Added The Popular Panel

--- src/MenuPage.js
+++ src/MenuPage.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
-import {Carousel} from "react-bootstrap"
-;
+import {Carousel,Panel} from "react-bootstrap"
+
 
 class Menu extends Component {
     render() {
       return (
+          <div>
         <div className="Menu">
         <Carousel>
   <Carousel.Item>
@@ -31,6 +32,32 @@ class Menu extends Component {
 </Carousel>;
           
         </div>
+        <br/>
+        <div className="Popular">
+        <Panel>
+          <Panel.Heading>Panel heading without a title</Panel.Heading>
+          <Panel.Body>Panel content</Panel.Body>
+          <img width={500} height={500} alt="900x500" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png" />
+    <Carousel.Caption>
+      <h3>First Item label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+    <img width={500} height={500} alt="900x500" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png" />
+    <Carousel.Caption>
+      <h3>Second Item label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+    
+    <img width={500} height={500} alt="900x500" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png" />
+    <Carousel.Caption>
+      <h3>Third Item label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+        </Panel>
+        
+        </div>
+
+</div>
       );
     }
   }
