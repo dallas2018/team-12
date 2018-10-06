81a9deb5cd731cc3b8fa87fb92a71ad69fb54b44
Tanzim Ali <tanzim_rayhan@yahoo.com>
2018-10-05 21:41:47 -0400

    Added the NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './navigation.js';
 
 class App extends Component {
   render() {
@@ -11,7 +12,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
+          <a>
+           < NavBar />
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
